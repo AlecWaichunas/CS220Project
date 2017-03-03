abecc7ff25b6c216e36a64684be2c99787756b16
Alec Waichunas <awaichunas@gmail.com>
2017-03-02 20:52:36 -0600

    Start of project

---
+++ src/siu/Main.java
@@ -0,0 +1,12 @@
+package siu;
+
+/**
+ * Created by Alec on 3/2/2017.
+ */
+public class Main {
+
+    public static void main(String args[]){
+        System.out.println("Hello World!");
+    }
+
+}
